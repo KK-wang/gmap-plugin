858fe3ecbb2d2bf60bac252a7ec7a68b6c4e8bf8
lorem <1021448137@qq.com>
2022-11-04 22:08:17 +0800

    complete the core logic

--- script/content-script.js
+++ script/content-script.js
@@ -9,7 +9,7 @@ const customizeStyle = document.createElement("style");
 customizeStyle.setAttribute("type", "text/css"); 
 customizeStyle.innerHTML = `
   .controlBar {
-   position: fixed;
+    position: fixed;
     top: 70px;
     right: 30px;
     z-index: 999;
@@ -43,7 +43,7 @@ heads[0].appendChild(bootstrapStyleLink);
 
 // HTML 部分如下：
 const controlBar = document.createElement("div");
-controlBar.classList.add("controlBar")
+controlBar.classList.add("controlBar");
 controlBar.innerHTML = `
   <div class="form-check form-switch">
     <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked>
@@ -59,27 +59,137 @@ window.$extension = {
 const controlBarInputEl = document.getElementById("flexSwitchCheckDefault");
 controlBarInputEl.addEventListener("change", () => {
   window.$extension.isAble = controlBarInputEl.checked;
+  directionButtonGroup.style.display = "none";
 });
 
 /* 创建四方按钮。*/
 // CSS 部分如下：
+const directionButtonGroupStyle = document.createElement("style");
+directionButtonGroupStyle.setAttribute("type", "text/css");
+directionButtonGroupStyle.innerHTML = `
+  #direction-button-group {
+    position: fixed;
+    z-index: 999;
+    display: none;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 90px;
+    height: 90px;
+  }
+  
+  #direction-button-group .first-line, 
+  #direction-button-group .second-line, 
+  #direction-button-group .third-line {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    width: 100%
+  }
+  
+  #direction-button-group .second-line {
+    justify-content: space-between;
+  }
+  
+  #direction-button-group .icon {
+    width: 33.33%;
+    height: 100%;
+    cursor: pointer;
+    
+  }
+  
+  #direction-button-group .icon {
+    width: 33.33%;
+    height: 100%;
+    cursor: pointer;
+    fill: #0d6efd;
+    transition: fill .25s;
+  }
+  
+  #direction-button-group .icon:hover {
+    fill: #699ce6;
+  }
+`;
+heads[0].appendChild(directionButtonGroupStyle);
 
 
 // HTML 部分如下：
-
+const directionButtonGroup = document.createElement("div");
+directionButtonGroup.classList.add("direction-button-group");
+directionButtonGroup.id = "direction-button-group";
+directionButtonGroup.innerHTML = `
+  <div class="first-line">
+    <svg class="top icon" style="transform: rotate(-90deg)" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path class="top" d="M569.470875 48.519234c0-42.375564 53.166368-67.895421 82.703239-30.088226l361.452537 463.374433a49.149353 49.149353 0 0 1 0 60.255217L652.174114 1005.592622c-29.536871 37.72843-82.703239 12.208573-82.703239-30.166991v-187.69697H74.98427C31.742291 787.728661 0 749.448876 0 705.419247V318.525618c0-44.029629 31.742291-82.388179 74.98427-82.388179H569.470875V48.519234z"></path></svg>
+  </div>
+  <div class="second-line">
+    <svg class="left icon" style="transform: rotate(180deg)" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path class="left" d="M569.470875 48.519234c0-42.375564 53.166368-67.895421 82.703239-30.088226l361.452537 463.374433a49.149353 49.149353 0 0 1 0 60.255217L652.174114 1005.592622c-29.536871 37.72843-82.703239 12.208573-82.703239-30.166991v-187.69697H74.98427C31.742291 787.728661 0 749.448876 0 705.419247V318.525618c0-44.029629 31.742291-82.388179 74.98427-82.388179H569.470875V48.519234z"></path></svg>
+    <svg class="right icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path class="right" d="M569.470875 48.519234c0-42.375564 53.166368-67.895421 82.703239-30.088226l361.452537 463.374433a49.149353 49.149353 0 0 1 0 60.255217L652.174114 1005.592622c-29.536871 37.72843-82.703239 12.208573-82.703239-30.166991v-187.69697H74.98427C31.742291 787.728661 0 749.448876 0 705.419247V318.525618c0-44.029629 31.742291-82.388179 74.98427-82.388179H569.470875V48.519234z"></path></svg>
+  </div>
+  <div class="third-line">
+    <svg class="bottom icon" style="transform: rotate(90deg)" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path class="bottom" d="M569.470875 48.519234c0-42.375564 53.166368-67.895421 82.703239-30.088226l361.452537 463.374433a49.149353 49.149353 0 0 1 0 60.255217L652.174114 1005.592622c-29.536871 37.72843-82.703239 12.208573-82.703239-30.166991v-187.69697H74.98427C31.742291 787.728661 0 749.448876 0 705.419247V318.525618c0-44.029629 31.742291-82.388179 74.98427-82.388179H569.470875V48.519234z"></path></svg>
+  </div>
+`;
+document.body.appendChild(directionButtonGroup);
 
 // JavaScript 部分如下：
-
-
-
+directionButtonGroup.addEventListener("click", (e) => {
+  if (revealCard.getBoundingClientRect().width === 0) {
+    alert("No location selected.");
+    return;
+  }
+  const itemClassList = Array.from(e.target.classList);
+  const res = getInfo();
+  if (res.imgUrl === null) {
+    alert("There are no pictures for this location.");
+    return;
+  }
+  const searchParams = new URLSearchParams(res.imgUrl.search);
+  searchParams.set("w", "1080");
+  searchParams.set("h", "720");
+  if (itemClassList.includes("top")) searchParams.set("yaw", "0");
+  else if (itemClassList.includes("left")) searchParams.set("yaw", "270");
+  else if (itemClassList.includes("right")) searchParams.set("yaw", "90");
+  else searchParams.set("yaw", "180");
+  res.imgUrl.search = searchParams.toString();
+  console.log(res.imgUrl.toString());
+});
 
 
 /* 事件监听逻辑。*/
 document.getElementById("scene").addEventListener("click", (e) => {
-  if (window.$extension.isAble) console.log("监听到了点击事件");
-  else console.log("没有监听到");
+  if (window.$extension.isAble) {
+    directionButtonGroup.style.top = `${e.offsetY - 45}px`;
+    directionButtonGroup.style.left = `${e.offsetX - 45}px`;
+    directionButtonGroup.style.display = "flex";
+  }
 });
 
+/* 获取 URL、坐标及地址名。*/
+const revealCard = document.querySelector("#reveal-card");
+function getInfo() {
+  let imgURL = null, addressText = null, coordinate = null;
+  try {
+    const imgButton = revealCard.querySelector("button[jstcache='608']");
+    const imgUrl = imgButton.style.backgroundImage.slice(5, -2);
+    const address = revealCard.querySelector("div[jstcache='624']:first-child")
+    addressText = address.textContent;
+    const coordinateButton = revealCard.querySelector("button[jstcache='627']:last-child");
+    coordinate = coordinateButton.textContent;
+    imgURL = new URL(imgUrl);
+  } catch (e) {
+    return {
+      imgUrl: null,
+      addressText,
+      coordinate,
+    };
+  }
+  return {
+    imgUrl: imgURL,
+    addressText,
+    coordinate,
+  }
+}
 
 /* 下载逻辑。*/
 
